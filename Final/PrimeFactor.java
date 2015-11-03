3a178bdc6fcc63283656a2be3899086baee7ad6b
Clinton <celliott0328@gmail.com>
2015-11-02 23:57:50 -0600

    Final

---
+++ Final/PrimeFactor.java
@@ -0,0 +1,52 @@
+package edu.iastate.cs228.hw3;
+
+/**
+ * 
+ * @author Clinton Elliott
+ *
+ */
+
+public class PrimeFactor {
+	public int prime; // prime factor
+	public int multiplicity; // number of times the prime factor appears in a
+								// factorization
+
+	/**
+	 * Precondition: p is a prime number.
+	 * 
+	 * @param p
+	 *            prime
+	 * @param m
+	 *            multiplicity
+	 * @throws IllegalArgumentException
+	 *             if m < 1
+	 */
+	public PrimeFactor(int p, int m) throws IllegalArgumentException {
+		if (m < 1) {
+			throw new IllegalArgumentException("Multiplicity less than one");
+		} else { // might not need else
+			prime = p;
+			multiplicity = m;
+		}
+	}
+
+	@Override
+	public PrimeFactor clone() {
+		return new PrimeFactor(prime, multiplicity);
+	}
+
+	/**
+	 * Prints out, for instance "2^3" if prime == 2 and multiplicity == 3, or
+	 * "5" if prime == 5 and multiplicity == 1.
+	 */
+	@Override
+	public String toString() {
+		String string = "";
+		if (multiplicity == 1) {
+			string += prime;
+		} else {
+			string += prime + "^" + multiplicity;
+		}
+		return string;
+	}
+}
